0ce6eaacab287951bcf16309ca8aaa8700333036
Thiago <diasthiago@protonmail.com>
2025-01-20 20:03:01 +0000

    Revert "test"
    This reverts commit 44a7e90b9d6780784bde622149f329052946cdfa.

--- kernel/sched/cpufreq_schedhorizon.c
+++ kernel/sched/cpufreq_schedhorizon.c
@@ -12,6 +12,7 @@
 
 #include <linux/sched/cpufreq.h>
 #include <trace/events/power.h>
+#include <trace/hooks/sched.h>
 
 static unsigned int default_efficient_freq[] = {0};
 static u64 default_up_delay[] = {0};
