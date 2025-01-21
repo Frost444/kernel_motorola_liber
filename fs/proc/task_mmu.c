2e84e5c0f860fc7b7b33b62f19f905c13571c1f7
Thiago <diasthiago@protonmail.com>
2025-01-21 12:08:43 +0000

    test build zygote

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -353,10 +353,6 @@ static void show_vma_header_prefix(struct seq_file *m,
 		   MAJOR(dev), MINOR(dev), ino);
 }
 
-static void show_vma_header_prefix_fake(struct seq_file *m,
-				   unsigned long start, unsigned long end,
-				   vm_flags_t flags, unsigned long long pgoff,
-				   dev_t dev, unsigned long ino)
 {
 	seq_setwidth(m, 25 + sizeof(void *) * 6 - 1);
 	seq_printf(m, "%08lx-%08lx %c%c%c%c %08llx %02x:%02x %lu ",
